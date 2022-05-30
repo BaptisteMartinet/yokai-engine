d8dde567bb8d29d59a722989227de1733921f727
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-05-30 23:42:56 +0200

    feat: rework Card

--- src/Card.ts
+++ src/Card.ts
@@ -21,49 +21,42 @@ export enum ColorsEnum {
   PURPLE
 }
 
-export enum HintEnum {
-  BLUE,
-  RED,
-  GREEN,
-  PURPLE,
-}
-
 export default class Card {
   private pos: Position2D;
   private color: ColorsEnum;
-  private hint: HintEnum | null;
+  private hint: number;
   private dragged: boolean;
   private revealed: boolean;
 
   constructor(_pos: Position2D, _color: ColorsEnum) {
     this.pos = _pos;
     this.color = _color;
-    this.hint = null;
+    this.hint = -1;
     this.dragged = false;
     this.revealed = false;
   }
 
-  get getPos() { return this.pos; }
-  get getColor() { return this.color; }
-  get getHint() { return this.hint; }
-  get getDragged() { return this.dragged; }
-  get getRevealed() { return this.revealed; }
+  get Pos() { return this.pos; }
+  get Color() { return this.color; }
+  get Hint() { return this.hint; }
+  get Dragged() { return this.dragged; }
+  get Revealed() { return this.revealed; }
 
-  set setPos(_newPos: { x: number, y: number }) {
+  set Pos(_newPos: { x: number, y: number }) {
     this.pos.x = _newPos.x;
     this.pos.y = _newPos.y;
   }
-  set setHint(_hint: HintEnum) {
-    if (this.hint)
+  set Hint(_hint: number) {
+    if (this.hint >= 0)
       throw new Error(`Hint already defined for card at pos ${this.pos}`);
     this.hint = _hint;
   }
-  set setDragged(_dragged: boolean) {
+  set Dragged(_dragged: boolean) {
     if (_dragged && this.dragged)
       throw new Error(`Card is already being dragged at pos ${this.pos}`);
     this.dragged = _dragged;
   }
-  set setRevealed(_revealed: boolean) {
+  set Revealed(_revealed: boolean) {
     if (_revealed && this.revealed)
       throw new Error(`Card is already revealed at pos ${this.pos}`);
     this.revealed = _revealed;
